88e1be4bec14a4b465cdd0a2f51e00b5c31360b9
suehiro41523 <67720039+suehiro41523@users.noreply.github.com>
2023-10-27 14:16:06 +0900

    update connect vue and laravel

---
+++ src/LaravelTestProject/resources/js/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="bg-slate-800 min-h-[100vh] text-white">
+        <div class="">
+            <!-- <Sidebar />
+            <TaskSection /> -->
+            <div class="">
+                <h2>Hello World</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+// import Sidebar from "./Sidebar.vue";
+// import TaskSection from "./TaskSection.vue";
+</script>
+<script>
+export default {
+    mounted() {
+        console.log("Component mounted.");
+    },
+};
+</script>
---
+++ src/LaravelTestProject/resources/views/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+
+    <!-- Vue -->
+    @vite(['resources/js/app.js','resources/css/app.css'])
+
+    <!-- Styles -->
+    <style>
+    </style>
+
+
+</head>
+
+<body class="">
+    <div id="app">
+    </div>
+
+</body>
+
+</html>
